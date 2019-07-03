f7bde11bded9242437cb504c6b81e349fb645c2e
A W <waq.mmhy@live.ca>
2019-07-02 20:53:11 -0400

    Reviews tableview, activity VC, alert VC, getting user input

--- Proj 4-6 Milestone Grocery List/ViewController.swift
+++ Proj 4-6 Milestone Grocery List/ViewController.swift
@@ -8,13 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // add nav bar item
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        navigationItem.rightBarButtonItems = [addButton, shareButton]
+
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(clearAll))
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GroceryItem", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter grocery item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+
+        shoppingList.insert(answer, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic) //animates new cell appearing
+        
+    }
+    
+    @objc func clearAll() {
+        shoppingList.removeAll()
+        tableView.reloadData()
+    }
+    
+    @objc func shareTapped() {
+        
+        let joinedList = shoppingList.joined(separator: "\n")
+        
+        let vc = UIActivityViewController(activityItems: [joinedList], applicationActivities: nil)
+        vc.modalPresentationStyle = .popover
+        present(vc, animated: true)
+    }
 
 }
 
